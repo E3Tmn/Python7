7d2907a324359b49ba5c6e16caaecc7a30206a77
E3Tmn <100991827+E3Tmn@users.noreply.github.com>
2023-08-21 15:52:30 +0300

    Update main.py
    Добавил вывод сообщения "Таймер запущен"

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def notify_progress(secs_left, author_id, message_id, message_time):
 
 
 def main(chat_id, message_time):
+    bot.send_message(chat_id,"Таймер запущен")
     message_id = bot.send_message(chat_id,
                                   f"Осталось {parse(message_time)} cекунд\n{render_progressbar(parse(message_time),0)}")
     time.sleep(1)
